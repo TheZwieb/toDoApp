10029d334ef987313d21ec6b7a46ddef97b7ebec
Austin <thezwieb@gmail.com>
2020-01-13 14:43:57 -0600

    Updates

---
+++ precache-manifest.7faa27a0ad293981a7e8c4732cd12150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b4ac6b7645edd0e07227733faab5d6",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "55a7fc40fe95f64e3a23",
+    "url": "/toDoApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83d80f0620c3e6202708",
+    "url": "/toDoApp/static/js/2.b97a247f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/toDoApp/static/js/2.b97a247f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55a7fc40fe95f64e3a23",
+    "url": "/toDoApp/static/js/main.6ba53e96.chunk.js"
+  },
+  {
+    "revision": "7a8905f1cf4a1a5ce0e0",
+    "url": "/toDoApp/static/js/runtime-main.d3ffb546.js"
+  }
+]);
